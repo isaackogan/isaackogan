d3b8ec4593ae753d7ea7b8d24cca72ba9beef730
Isaac Kogan <isaacikogan@gmail.com>
2022-02-24 19:48:01 -0500

    Update Resource Order

--- src/components/redirect/UkraineRedirects.js
+++ src/components/redirect/UkraineRedirects.js
@@ -67,18 +67,17 @@ function RedirectButton({name = "Button", href = "#", target="", style={}}) {
 class UkraineRedirects extends Component {
 
     render() {
-
         return (
             <ContainedRedirect className={`defaultGrey`}>
                 <RedirectButton
                     style={{"backgroundColor": "rgb(16,53,182)", "borderTopRightRadius": "1em", "borderTopLeftRadius": "1em", "marginTop": "15px"}}
-                    name="UJA Emergency Relief Fund" href="https://jewishtoronto.com/ukraine-emergency-relief-fund?utm_source=emergency+campaign+&utm_medium=email&utm_campaign=Feb_24_22_Ukraine_Emergency_Community_Email" target="_blank" logo="/logos/tiktok.svg"/>
+                    name="Stand With Ukraine (ukraine.ua)" href="https://ukraine.ua/news/stand-with-ukraine/" target="_blank" logo="/logos/tiktok.svg"/>
                 <RedirectButton
                     style={{"background": "linear-gradient(to bottom, rgb(16,53,182) 50%, rgb(229,182,11) 50%)", "borderRadius": "0"}}
                     name="UNICEF Emergency Relief Fund" href="https://secure.unicef.ca/page/98630/donate/1?ea.tracking.id=22DIEM02GSE&gclid=CjwKCAiA9tyQBhAIEiwA6tdCrIsYdCP1VgLLZzEfJglNrmd_DpqoQ59l4ZWVpyjJQ4URgXQJ_G3lhhoCsLQQAvD_BwE" target="_blank" logo="/logos/tiktok.svg"/>
                 <RedirectButton
                     style={{"backgroundColor": "rgb(229,182,11)", "borderBottomLeftRadius": "1em", "borderBottomRightRadius": "1em"}}
-                    name="Stand With Ukraine (ukraine.ua)" href="https://ukraine.ua/news/stand-with-ukraine/" target="_blank" logo="/logos/tiktok.svg"/>
+                    name="UJA Emergency Relief Fund" href="https://jewishtoronto.com/ukraine-emergency-relief-fund?utm_source=emergency+campaign+&utm_medium=email&utm_campaign=Feb_24_22_Ukraine_Emergency_Community_Email" target="_blank" logo="/logos/tiktok.svg"/>
             </ContainedRedirect>
         )
     }
